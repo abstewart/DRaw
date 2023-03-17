4be80f9c9fe7df14c8ff6db758766f8d9dd61bf4
StevenAbbott <sbabbott08@gmail.com>
2023-03-17 06:17:14 -0400

    Managing coverage data in D is *brutal*. All of the coverage file parsing/summarizing libraries -and I mean all of them; all 8- are out of date

---
+++ client/source/test_example_1.d
@@ -0,0 +1,5 @@
+/// Test stuff 1!
+@("some other test in test.d")
+unittest{
+    assert(2==2,"2==2 should pass");
+}
---
+++ client/source/test_example_2.d
@@ -0,0 +1,5 @@
+/// Test stuff 2! (to ensure the multithreading of unit-threaded is working!)
+@("some other test in test2.d")
+unittest{
+    assert(3==3,"3==3 should pass");
+}
