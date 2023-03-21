66fd39d659b15ec81d5853b16774cabd6aea6a86
StevenAbbott <sbabbott08@gmail.com>
2023-03-21 17:46:57 -0400

    Add all necessary files to server dub project, add server dub project to Github Actions

--- client/source/app.d
+++ client/source/app.d
@@ -3,5 +3,5 @@ import std.stdio;
 /// Main function. Entry point for the program.
 void main()
 {
-    writeln("Edit source/app.d to start your project.");
+    writeln("This is the client! Hello!!!");
 }
