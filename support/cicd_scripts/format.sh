9e0c560ea31235dbd5a48ece1379633c87e90ce1
StevenAbbott <sbabbott08@gmail.com>
2023-03-21 04:34:48 -0400

    Finalize script to check formatting, implement unit tests and format checking into Github Actions

--- support/cicd_scripts/format.sh
+++ support/cicd_scripts/format.sh
@@ -1,10 +1,56 @@
 #!/bin/bash
+###
+### Uses the dfmt library to check if all .d files in the given directory are
+### properly formatted. If they are - succeed, if not - fail.
+###
+
+# Look for .d files and store temporary state in the given file:
+SEARCH_FOLDER=$1
+D_COPY_DIR="d_file_copies"
+D_COPY_PATH=$SEARCH_FOLDER/$D_COPY_DIR
+
+# If a previously-made copy folder exists, wipe it:
+if [ -d $D_COPY_PATH ];
+then
+  rm -r $D_COPY_PATH
+fi
+
+# Make a new copy folder to run dfmt inside:
+mkdir $D_COPY_PATH
+
+# Find all .d files:
+DFILES=`find $SEARCH_FOLDER -name '*.d'`
 
 
-# Find all .d files
-DFILES=`find ./ -name '*.d'`
-
-
+# Copy all files over:
 for val in $DFILES; do
-  echo $val
+  cp $val $D_COPY_PATH/$(basename $val)
 done
+
+# Run the formatter on the copied files:
+dub run dfmt --yes -- --version
+for val in $D_COPY_PATH; do
+  dub run dfmt -- -i $val
+done
+
+# Record if any of the files are different so we can fail later:
+ANY_DIFFERENT=False
+for val in $DFILES; do
+  COPIED_PATH=$D_COPY_PATH/$(basename $val)
+  cmp -s $val $COPIED_PATH || echo `cmp $val $COPIED_PATH` || ANY_DIFFERENT=True
+done
+
+# Wipe the copy file:
+if [ -d $D_COPY_PATH ];
+then
+  rm -r $D_COPY_PATH
+fi
+
+# End with error if there were any differences:
+if [ $ANY_DIFFERENT ];
+then
+  set -e
+fi
+
+# There were no differences! Success!
+exit
