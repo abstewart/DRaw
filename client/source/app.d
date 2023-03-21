826150c472e468d47064d8c29900eb4a7f1215e6
StevenAbbott <sbabbott08@gmail.com>
2023-03-21 16:47:46 -0400

    Remove coverage test code in main

--- client/source/app.d
+++ client/source/app.d
@@ -1,34 +1,7 @@
 import std.stdio;
 
-/// Create a Singleton to store global configuration variables
-class Singleton{
-    private static Singleton instance;
-    int parachuteType;
-    int landingZone;
-
-    /// Private constructor
-    private this(){}
-
-    /// Retrieves an instance of this singleton while ensuring that only one is ever created
-    static Singleton GetInstance(){
-        if(instance is null){
-            instance = new Singleton;
-        }
-
-        return instance;
-    }
-
-    /// Reset the values in the singleton
-    void reset(){
-        parachuteType = 0;
-        landingZone = 0;
-    }
-}
-
 /// Main function. Entry point for the program.
 void main()
 {
-    Singleton meep = Singleton.GetInstance();
-
     writeln("Edit source/app.d to start your project.");
 }
