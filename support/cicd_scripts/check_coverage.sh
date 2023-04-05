0a7c388cced8234337904131311e3c43ad48c4c0
Steven Abbott <abbott.st@northeastern.edu>
2023-04-05 13:50:34 -0400

    Enable check coverage to spot sub-100 percentages

--- support/cicd_scripts/check_coverage.sh
+++ support/cicd_scripts/check_coverage.sh
@@ -4,6 +4,9 @@
 ### (or contains no code), otherwise, error.
 ###
 
+# Coverage target:
+COVERAGE_TARGET="20"
+
 # Look for .d files and store temporary state in the given file:
 SEARCH_FOLDER=$1
 COV_DIR="coverage"
@@ -14,22 +17,42 @@ COV_FILES=`find $COV_PATH/* -name '*.lst'`
 echo Code coverage report:
 
 # Ensure that the /coverage directory is not empty:
-if [ -z "$(ls -A COV_PATH)" ];
+if [ -z "$(ls -A $COV_PATH)" ];
 then
   echo Failed! No coverage whatsoever!
   exit 1
 fi
 
 ANY_UNDER_TARGET=false
+ANY_EMPTY=false
 for val in $COV_FILES; do
   LAST_LINE=$(tail -1 $val)
   LAST_CHARS=$(tail -c 13 $val)
-  echo $LAST_LINE
-  if [[ "$LAST_CHARS" != " has no code" ]] && [[ "$LAST_CHARS" != "100% covered" ]];
+
+  if [ "$LAST_CHARS" = "" ]; then
+    ANY_EMPTY=true
+    echo "Empty file! $val"
+  fi
+
+  if [ $ANY_EMPTY ] && [ "$LAST_CHARS" != " has no code" ] && [ "$LAST_CHARS" != "100% covered" ];
   then
-    ANY_UNDER_TARGET=true
+    if [ "$LAST_CHARS" = "s 0% covered" ];
+    then
+      echo "0%: $val"
+      ANY_UNDER_TARGET=true
+    else
+      if [ "$(echo $LAST_CHARS | tr -d '%' | head -c 2)" -lt $COVERAGE_TARGET ]; then
+        echo "below $COVERAGE_TARGET: $val"
+        ANY_UNDER_TARGET=true
+      fi
+    fi
   fi
 done
+
+if $ANY_EMPTY; then
+  echo "Failed! Delete empty .d files, they generate empty coverage reports which are annoying to deal with!"
+  exit 1
+fi
 
 # End with error if there were any files under the coverage limit:
 if $ANY_UNDER_TARGET;
