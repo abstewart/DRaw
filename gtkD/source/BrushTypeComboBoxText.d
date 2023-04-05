c5fbab64f18d4e6028d67a07bed745703e3a73f2
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2023-04-05 16:36:15 -0400

    Redid the ComboBoxText for the brush types so the users cannot type an invalid brush type in. Created BrushTypeComboBoxText.d.

---
+++ gtkD/source/BrushTypeComboBoxText.d
@@ -0,0 +1,29 @@
+// Imports.
+private import MyDrawing : MyDrawing;
+
+private import gtk.ComboBoxText;                        // CombBoxText.
+
+/// Class representing the ComboBoxText that is made up of the brush types.
+class BrushTypeComboBoxText : ComboBoxText {
+    // Instance variables.
+    private:
+    string[] brushTypes = ["Filled Arc", "Arc", "Line", "Point", "Rectangle", "Filled Rectangle"];
+    bool entryOn = false;
+
+    /// Constructor.
+    public:
+    this(MyDrawing drawingArea) {
+        super(entryOn);
+
+        // Add the brush types to the BrushTypeComboBoxText.
+        foreach (brush; brushTypes) {
+            appendText(brush);
+        }
+
+        // Preselect the first item in the drop-down.
+        setActive(0);
+
+        // How the widge signal addOnChanged() is harnessed.
+        addOnChanged(&drawingArea.onBrushOptionChanged);
+    }
+}
--- gtkD/source/MyDrawingBox.d
+++ gtkD/source/MyDrawingBox.d
@@ -4,11 +4,11 @@ private import std.stdio;                               // writeln.
 private import MyDrawing : MyDrawing;
 private import MyColorChooserDialog : MyColorChooserDialog;
 private import Command : Command;
+private import BrushTypeComboBoxText : BrushTypeComboBoxText;
 
 private import gtk.VBox;                                // VBox.
 private import gtk.Button;                              // Button.
 private import gtk.Label;                               // Label.
-private import gtk.ComboBoxText;                        // CombBoxText.
 private import gtk.HBox;                                // HBox.
 
 /// Class representing the box that the users drawing sits in. Includes the brush type, brush size, color picker, undo, and save options.
@@ -21,21 +21,11 @@ class MyDrawingBox : VBox {
     /// Constructor.
     public:
     this() {
-        super(false, 4);
+        super(false, 4);                                     // this(bool homogeneous, int spacing).
         writeln("MyDrawingBox constructor");
 
         this.drawingArea = new MyDrawing();
-
-        ComboBoxText primOption = new ComboBoxText();
-        primOption.appendText("Filled Arc");
-        primOption.appendText("Arc");
-        primOption.appendText("Line");
-        primOption.appendText("Point");
-        primOption.appendText("Rectangle");
-        primOption.appendText("Filled Rectangle");
-        primOption.setActive(0);
-        primOption.addOnChanged(&this.drawingArea.onBrushOptionChanged);
-
+        BrushTypeComboBoxText brushTypes = new BrushTypeComboBoxText(this.drawingArea);
         packStart(this.drawingArea, true, true, 0);          // Adds child to box, packed with reference to the start of box.
 
         Button colorButton = new Button("Color Dialog", &showColor);
@@ -44,7 +34,7 @@ class MyDrawingBox : VBox {
 
         HBox hbox = new HBox(false, 4);
         hbox.packStart(new Label("Brush type"), false, false, 2);
-        hbox.packStart(primOption, false, false, 2);
+        hbox.packStart(brushTypes, false, false, 2);
         hbox.packStart(new Label("Brush size"), false, false, 2);
         hbox.packStart(this.drawingArea.getSpin(), false, false, 2);
         hbox.packStart(colorButton, false, false, 2);
