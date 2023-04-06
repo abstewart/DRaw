61910aa118e49fa9d9eed7d4881371c289c7bd59
Steven Abbott <abbott.st@northeastern.edu>
2023-04-06 06:31:04 -0400

    Remove mv command from check_docs to cater to GA

--- support/cicd_scripts/check_docs.sh
+++ support/cicd_scripts/check_docs.sh
@@ -9,29 +9,59 @@ DOC_COPY_DIR="doc_copies"
 ORIGINAL_DOC_PATH=$SEARCH_FOLDER/docs
 DOC_COPY_PATH=$SEARCH_FOLDER/$DOC_COPY_DIR
 
-mv $ORIGINAL_DOC_PATH $DOC_COPY_PATH
+#mv $ORIGINAL_DOC_PATH $DOC_COPY_PATH
 
-#dub --build=docs
 
-#DOC_FILES=`find $ORIGINAL_DOC_PATH -name '*.html'`
-#
-## Record if any of the files are different so we can fail later:
-#ANY_DIFFERENT=false
-#for val in $DOC_FILES; do
-#  COPY_PATH=$DOC_COPY_PATH/$(basename $val)
-#  if ! cmp -s $val $COPY_PATH; then
-#    cmp $val $COPY_PATH
-#    ANY_DIFFERENT=true
-#  fi
-#done
-#
-## Wipe the copy file:
-#if [ -d $ORIGINAL_DOC_PATH ];
-#then
-#  rm -r $ORIGINAL_DOC_PATH
-#fi
-#
+if [ -d $DOC_COPY_PATH ];
+then
+  rm -r $DOC_COPY_PATH
+fi
+
+mkdir $DOC_COPY_PATH
+
+DOC_FILES=`find $ORIGINAL_DOC_PATH -name '*.html'`
+
+# Copy all files over:
+for val in $DOC_FILES; do
+  cp $val $DOC_COPY_PATH/$(basename $val)
+done
+
+
+dub --build=docs
+
+
+# Record if any of the files are different so we can fail later:
+ANY_DIFFERENT=false
+for val in $DOC_FILES; do
+  COPY_PATH=$DOC_COPY_PATH/$(basename $val)
+  if ! cmp -s $val $COPY_PATH; then
+    cmp $val $COPY_PATH
+    ANY_DIFFERENT=true
+  fi
+done
+
+# Wipe the copy file:
+if [ -d $ORIGINAL_DOC_PATH ];
+then
+  rm -r $ORIGINAL_DOC_PATH
+fi
+
+
+
+mkdir $ORIGINAL_DOC_PATH
+
+COPIED_DOC_FILES=`find $DOC_COPY_PATH -name '*.html'`
+
+# Copy all files over:
+for val in $COPIED_DOC_FILES; do
+  cp $val $ORIGINAL_DOC_PATH/$(basename $val)
+done
+
+
 #mv $DOC_COPY_PATH $ORIGINAL_DOC_PATH
+
+
+
 
 # End with error if there were any differences:
 if $ANY_DIFFERENT;
