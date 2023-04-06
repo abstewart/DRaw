ec319f34bb878db9646c0e82da619cd4749007e0
Steven Abbott <abbott.st@northeastern.edu>
2023-04-06 06:18:22 -0400

    gut check_docs to test if dub is an issue in shell scripts in GA

--- support/cicd_scripts/check_docs.sh
+++ support/cicd_scripts/check_docs.sh
@@ -11,27 +11,27 @@ DOC_COPY_PATH=$SEARCH_FOLDER/$DOC_COPY_DIR
 
 mv $ORIGINAL_DOC_PATH $DOC_COPY_PATH
 
-dub --build=docs
+#dub --build=docs
 
-DOC_FILES=`find $ORIGINAL_DOC_PATH -name '*.html'`
-
-# Record if any of the files are different so we can fail later:
-ANY_DIFFERENT=false
-for val in $DOC_FILES; do
-  COPY_PATH=$DOC_COPY_PATH/$(basename $val)
-  if ! cmp -s $val $COPY_PATH; then
-    cmp $val $COPY_PATH
-    ANY_DIFFERENT=true
-  fi
-done
-
-# Wipe the copy file:
-if [ -d $ORIGINAL_DOC_PATH ];
-then
-  rm -r $ORIGINAL_DOC_PATH
-fi
-
-mv $DOC_COPY_PATH $ORIGINAL_DOC_PATH
+#DOC_FILES=`find $ORIGINAL_DOC_PATH -name '*.html'`
+#
+## Record if any of the files are different so we can fail later:
+#ANY_DIFFERENT=false
+#for val in $DOC_FILES; do
+#  COPY_PATH=$DOC_COPY_PATH/$(basename $val)
+#  if ! cmp -s $val $COPY_PATH; then
+#    cmp $val $COPY_PATH
+#    ANY_DIFFERENT=true
+#  fi
+#done
+#
+## Wipe the copy file:
+#if [ -d $ORIGINAL_DOC_PATH ];
+#then
+#  rm -r $ORIGINAL_DOC_PATH
+#fi
+#
+#mv $DOC_COPY_PATH $ORIGINAL_DOC_PATH
 
 # End with error if there were any differences:
 if $ANY_DIFFERENT;
