c8169b208838741c4c66d5a80be51620f03fdd40
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2023-04-14 11:54:56 -0400

    Uncommented out client_cov_app.d, client_move_coverage_files.d, server_cov_app.d, and server_move_coverage_files.d.

--- client-and-server/client/source/client_cov_app.d
+++ client-and-server/client/source/client_cov_app.d
@@ -1,8 +1,8 @@
-//// Imports.
-//private import std.stdio; // writeln.
-//
-///// Main method -- run the application. Entry point for the program.
-//void main(string[] args)
-//{
-//    writeln("Starting the DRaw client.");
-//}
+// Imports.
+private import std.stdio; // writeln.
+
+/// Main method -- run the application. Entry point for the program.
+void main(string[] args)
+{
+    writeln("Starting the DRaw client.");
+}
--- client-and-server/server/source/server_cov_app.d
+++ client-and-server/server/source/server_cov_app.d
@@ -1,8 +1,8 @@
-//// Imports.
-//private import std.stdio; // writeln.
-//
-///// Main method -- run the application. Entry point for the program.
-//void main(string[] args)
-//{
-//    writeln("Starting the DRaw server.");
-//}
+// Imports.
+private import std.stdio; // writeln.
+
+/// Main method -- run the application. Entry point for the program.
+void main(string[] args)
+{
+    writeln("Starting the DRaw server.");
+}
