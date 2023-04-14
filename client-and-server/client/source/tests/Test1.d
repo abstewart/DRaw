3ed03fd76ce679b969a46e5a03fbe68ea19bd2c2
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2023-04-14 15:31:30 -0400

    Wrote first test -- Testing getting and setting connection status

--- client-and-server/client/source/tests/Test1.d
+++ client-and-server/client/source/tests/Test1.d
@@ -2,9 +2,28 @@
 module tests.Test1;
 
 // Imports.
+private import gtkd.Loader;
 
+private import view.MyWindow;
 
-@("Test 1 -- Testing")
+private import gio.Application : GioApplication = Application; // GioApplication.
+
+private import gtk.Application; // Application.
+
+@("Test 1 -- Testing getting and setting connection status")
 unittest {
-    assert(1 == 1);
+    Linker.dumpLoadLibraries();
+    Linker.dumpFailedLoads();
+    auto application = new Application("dRaw.project", GApplicationFlags.FLAGS_NONE);
+    MyWindow myWindow;
+    assert(!application.getIsRegistered());
+    application.register(null);
+    assert(application.getIsRegistered());
+    application.addOnActivate(delegate void(GioApplication app) {
+        myWindow = new MyWindow(application);
+    });
+    application.activate();
+    assert(!myWindow.getConnection);
+    myWindow.setConnection(true);
+    assert(myWindow.getConnection);
 }
