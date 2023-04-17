43695d25c1d5148e5830679a89bf0e833108ee25
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2023-04-17 19:36:24 -0400

    Added a period to a string in ConnectDialog.d.

--- client-and-server/client/source/view/components/ConnectDialog.d
+++ client-and-server/client/source/view/components/ConnectDialog.d
@@ -92,7 +92,7 @@ public:
         MessageDialog message = connected ? new MessageDialog(this, GtkDialogFlags.MODAL,
                 MessageType.INFO, ButtonsType.OK, "You are now connceted!") : new MessageDialog(this,
                 GtkDialogFlags.MODAL,
-                MessageType.INFO, ButtonsType.OK, "Connection failed, please try again");
+                MessageType.INFO, ButtonsType.OK, "Connection failed, please try again.");
         message.run();
         message.destroy();
 
